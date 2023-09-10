080bbb3cd048ef0da474e590ccb28df7fa6bf94f
Niviammu17 <144579500+Niviammu17@users.noreply.github.com>
2023-09-10 15:18:25 +0530

    Create Leapyear.py
    To leap year of a given number

---
+++ Leapyear.py
@@ -0,0 +1,23 @@
+#leap year
+
+"""
+year %4==0&
+year%100!=0/
+year%400==0
+
+"""
+def is leapyear (year):
+  if (year %4==0 and year%100!=0) or year %400==0:
+    return true
+  else:
+    return false
+
+year =int(input("enter a year :"))
+
+if isleapyear(year):
+ print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a leapyear.'. format(year)
+
+
+        
